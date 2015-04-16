43d5fb340a7ad1f2f3a26b2ae7733b90f5b12645
David Yao <xinyao@cn.ibm.com>
2015-04-16 09:57:14 +0800

    first commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bloggerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the bloggerApp
+ */
+angular.module('bloggerApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+    $scope.appName="Blogger"
+  });
